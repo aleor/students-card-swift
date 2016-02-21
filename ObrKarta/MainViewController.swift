e0e467ca58d0eb2f424ec4dc6ccb3d723c902e05
RA <arvalea@gmail.com>
2016-02-21 23:25:00 +0300

    init ui

---
+++ ObrKarta/MainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainViewController.swift
+//  ObrKarta
+//
+//  Created by RA on 21/02/16.
+//  Copyright © 2016 arvalea.com. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var PasswordTextBox: UITextField!
+    
+    @IBOutlet weak var loginTextBox: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func checkBalanceButton(sender: UIButton) {
+        
+        let login = loginTextBox.text
+        
+        let password = PasswordTextBox.text
+        
+        resultLabel.text = login! + " " + password!
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+   }
